8c3ba269ac053ad63aed2ebc5181b3652314cd87
TessLMAO <110305665+TessLMAO@users.noreply.github.com>
2022-11-06 18:22:54 -0300

    Add files via upload

---
+++ assets/preload/data/madness/script.lua
@@ -0,0 +1,15 @@
+function onStepHit() -- subtitles with lua
+	
+		if curStep == 1520 then
+			triggerEvent('karaokelyrics','set','HAHA.');
+		end
+		if curStep == 1527 then
+			
+			triggerEvent('karaokelyrics','set','HAHA. BITCH.');
+			triggerEvent('karaokelyrics','sung','HAHA. BITCH.');
+		end
+		if curStep == 1537 then
+			triggerEvent('karaokelyrics','set',' ');
+		end
+	
+end
